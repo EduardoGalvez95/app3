7398bc4be1143b1b79e20661966c3f30be6e4258
EduardoGalvez95 <eduardoacar.galvez@uteq.edu.ec>
2021-08-03 08:00:11 -0500

    version 1

---
+++ src/com/ecodeup/conexion/Conexion.java
@@ -0,0 +1,29 @@
+package com.ecodeup.conexion;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+public class Conexion {
+	private static DriverManagerDataSource dataSource=null;
+	
+	private static DataSource getDataSource() {
+		if (dataSource==null) {
+			dataSource= new DriverManagerDataSource();
+			dataSource.setDriverClassName("org.postgresql.Driver");
+			dataSource.setUsername("postgres");
+			dataSource.setPassword("admin");
+			dataSource.setUrl("jdbc:postgresql://34.95.240.179:5432/postgres");
+                        //psql "sslmode=verify-ca sslrootcert=server-ca.pem sslcert=client-cert.pem sslkey=client-key.pem hostaddr=34.95.240.179 port=5432 user=postgres dbname=postgres"
+		}
+		return dataSource;
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return getDataSource().getConnection();
+	}
+}
